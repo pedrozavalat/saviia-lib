e76c905a57370a16af846a94040d39a4a0c397ff
PEDRO ZAVALA <pedropablozavalat@uc.cl>
2025-10-17 16:56:07 -0300

    refactor: apply lint

--- src/saviialib/services/netcamera/api.py
+++ src/saviialib/services/netcamera/api.py
@@ -17,6 +17,7 @@ class NetcameraAPI:
         self, cameras: Dict[str, Tuple[str, int]]
     ) -> Dict[str, Any]:
         """Retrieve media files from Network cameras.
+
         :param cameras: Dictionary where the key is the identifier of the camera, and the
             value is a tuple wich contains the service IP address and port of connection.
             Example: {'cam_01': ('192.168.1.10', 8080), ...}
